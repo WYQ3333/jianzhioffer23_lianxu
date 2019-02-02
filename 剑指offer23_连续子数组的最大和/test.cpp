688c68538c79f35717365103fd8247fd2ea20071
浅忆梦微凉 <3538922590@qq.com>
2019-02-02 21:42:35 +0800

    新方法

--- 剑指offer23_连续子数组的最大和/test.cpp
+++ 剑指offer23_连续子数组的最大和/test.cpp
@@ -2,27 +2,44 @@
 #include<vector>
 using namespace std;
 
+//class Solution {
+//public:
+//	int FindGreatestSumOfSubArray(vector<int> array) {
+//		int result = 0;
+//		int temp = 0;
+//		int i = 0;
+//		for (i = 0; i < array.size(); ++i){
+//			result += array[i];
+//		}
+//		i = 0;
+//		while (i < array.size()){
+//			for (int j = i; j < array.size(); ++j){
+//				temp += array[j];
+//				if (temp >= result){
+//					result = temp;
+//				}
+//			}
+//			temp = 0;
+//			++i;
+//		}
+//		return result;
+//	}
+//};
+
 class Solution {
 public:
 	int FindGreatestSumOfSubArray(vector<int> array) {
-		int result = 0;
-		int temp = 0;
-		int i = 0;
-		for (i = 0; i < array.size(); ++i){
-			result += array[i];
+
+		int cursum = array[0];
+		int maxsum = array[0];
+		for (int i = 1; i<array.size(); i++){
+			cursum += array[i];
+			if (cursum<array[i])
+				cursum = array[i];
+			if (cursum>maxsum)
+				maxsum = cursum;
 		}
-		i = 0;
-		while (i < array.size()){
-			for (int j = i; j < array.size(); ++j){
-				temp += array[j];
-				if (temp >= result){
-					result = temp;
-				}
-			}
-			temp = 0;
-			++i;
-		}
-		return result;
+		return maxsum;
 	}
 };
 
