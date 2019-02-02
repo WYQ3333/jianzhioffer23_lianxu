4a024e6a9810c8710789c4bc3b4ffeb335c180c6
浅忆梦微凉 <3538922590@qq.com>
2019-02-02 21:27:59 +0800

    编译通过

--- 剑指offer23_连续子数组的最大和/test.cpp
+++ 剑指offer23_连续子数组的最大和/test.cpp
@@ -4,28 +4,16 @@ using namespace std;
 
 class Solution {
 public:
-	int TheSum(int plow, int phigh,vector<int> array){
-		int result = 0;
-		for (int i = plow; i < phigh; ++i){
-			result += array[i];
-		}
-		return result;
-	}
-
 	int FindGreatestSumOfSubArray(vector<int> array) {
 		int result = 0;
-		result = TheSum(0, array.size() - 1, array);
-		int plow = 0;
-		int phigh = array.size() - 1;
-		while (phigh > plow){
-			int temp = TheSum(plow, phigh, array);
+		int temp = 0;
+		for (int i = 0; i < array.size(); ++i){
+			temp += array[i];
 			if (temp >= result){
 				result = temp;
 			}
-			else if (temp < result){
-				continue;
-			}
 		}
+		return result;
 	}
 };
 
